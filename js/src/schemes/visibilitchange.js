7ce8ec878cb7437c767a6ac1736d44097449b5b0
alluretear <784365469@QQ.com>
2017-10-25 17:38:40 +0800

    Site updated: 2017-10-25 17:38:39

--- js/src/schemes/visibilitchange.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('visibilitychange', function() {
-  var isHidden = document.hidden;
-  if (isHidden) {
-    document.title = '当焦点不在当前窗口时的网页标题';
-  } else {
-    document.title = '再变回来或者做点其他的';
-  }
-});
